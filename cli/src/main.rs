eb901aa0e9c85b5d0f3e5f1389002a8d44feeaca
Fadi Atieh <fadi.atieh98@gmail.com>
2024-03-30 08:36:40 -0500

    Add some cruft to cli.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,12 @@
 use nn_core::matrix;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 
 fn main() {
-    let matrix = matrix::Matrix::zeros((2, 2));
+    let seed = [42; 32]; // A fixed seed for reproducibility
+    let mut rng = StdRng::from_seed(seed);
+    let generator = || rng.gen();
 
+    let matrix = matrix::Matrix::random((2, 2), generator);
     println!("{}", matrix);
 }
