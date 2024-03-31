e0b1d3ef27060e8c3760d41d3b6746ca085bf99b
Fadi Atieh <fadi.atieh98@gmail.com>
2024-03-31 08:09:25 -0500

    Write documentation for activation function>

--- nn_core/src/layer/activation.rs
+++ nn_core/src/layer/activation.rs
@@ -1,29 +1,39 @@
+/// This module contains the implementation of activation functions and an activation layer.
+/// Activation functions are mathematical functions that introduce non-linearity to neural networks.
+/// The activation layer applies an activation function element-wise to the input matrix.
+/// Supported activation functions are Sigmoid, ReLU, and Tanh.
 use crate::layer::Layer;
 use crate::matrix::Matrix;
 
+/// Enum representing different activation functions.
 pub enum ActivationFunction {
     Sigmoid,
     ReLU,
     Tanh,
 }
 
+/// Computes the sigmoid function for a given input.
 fn sigmoid(x: f32) -> f32 {
     1.0 / (1.0 + (-x).exp())
 }
 
+/// Computes the hyperbolic tangent function for a given input.
 fn tanh(x: f32) -> f32 {
     x.tanh()
 }
 
+/// Computes the rectified linear unit function for a given input.
 fn relu(x: f32) -> f32 {
     x.max(0.0)
 }
 
+/// Struct representing an activation layer.
 pub struct ActivationLayer {
     activation_function: ActivationFunction,
 }
 
 impl ActivationLayer {
+    /// Creates a new activation layer with the specified activation function.
     pub fn new(activation_function: ActivationFunction) -> Self {
         ActivationLayer {
             activation_function,
@@ -32,6 +42,7 @@ impl ActivationLayer {
 }
 
 impl Layer for ActivationLayer {
+    /// Applies the activation function element-wise to the input matrix and returns the result.
     fn forward(&self, input: &Matrix) -> Matrix {
         let data = (0..input.get_shape().0)
             .flat_map(|i| (0..input.get_shape().1).map(move |j| (i, j)))
@@ -48,8 +59,6 @@ impl Layer for ActivationLayer {
         Matrix::new(data, input.get_shape())
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
