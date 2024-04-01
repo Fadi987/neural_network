52a9a76bb14c6bc5ee88e1f7a828118a7d043306
Fadi Atieh <fadi.atieh98@gmail.com>
2024-04-01 06:43:46 -0500

    Add test case for forward pass.

--- nn_core/src/neural_network.rs
+++ nn_core/src/neural_network.rs
@@ -22,3 +22,30 @@ impl NeuralNetwork {
             .fold(input.clone(), |acc, layer| layer.forward(&acc))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::layer::{activation, fully_connected, Layer};
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+
+    #[test]
+    fn test_neural_network_forward() {
+        let mut network = NeuralNetwork::new();
+        let seed = [42; 32];
+        let rng = StdRng::from_seed(seed);
+
+        let fully_connected_layer = fully_connected::FullyConnectedLayer::new((2, 3), rng);
+        let activation_layer =
+            activation::ActivationLayer::new(activation::ActivationFunction::ReLU);
+
+        network.add_layer(fully_connected_layer);
+        network.add_layer(activation_layer);
+
+        let input = matrix::Matrix::new(vec![1.0, 2.0, 3.0], (3, 1));
+        let output = network.forward(&input);
+
+        assert_eq!(output.get_shape(), (2, 1));
+    }
+}
