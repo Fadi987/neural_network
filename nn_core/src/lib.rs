822797a4f8756276be8c56ecf770704cd65adcd3
Fadi Atieh <fadi.atieh98@gmail.com>
2024-04-07 10:24:07 -0500

    Expose Matrix::zeros to Python.

--- nn_core/src/lib.rs
+++ nn_core/src/lib.rs
@@ -1,3 +1,12 @@
 pub mod matrix;
 pub mod neural_network;
 pub mod optimizer;
+mod py_bindings;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn nn_core(_: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<py_bindings::PyMatrix>()?;
+    Ok(())
+}
