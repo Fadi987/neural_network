8f2badd7426fa0f87d8caae38847a4c640e473a5
Fadi Atieh <fadi.atieh98@gmail.com>
2024-04-10 07:31:11 -0500

    Expose various method of matrix.

--- py_nn/src/pymatrix.rs
+++ py_nn/src/pymatrix.rs
@@ -1,6 +1,7 @@
 use nn_core::matrix;
 use pyo3::prelude::*;
-use pyo3::types::PyTuple;
+use pyo3::types::PyFunction;
+use pyo3::Python;
 
 #[pyclass]
 pub struct PyMatrix {
@@ -10,22 +11,73 @@ pub struct PyMatrix {
 #[pymethods]
 impl PyMatrix {
     #[staticmethod]
-    pub fn zeros(shape: &Bound<'_, PyTuple>) -> Self {
-        let shape: (usize, usize) = shape.extract().unwrap();
+    pub fn zeros(shape: (usize, usize)) -> Self {
         PyMatrix {
             matrix: matrix::Matrix::zeros(shape),
         }
     }
 
-    #[new]
-    pub fn new(shape: &Bound<'_, PyTuple>, value: f32) -> Self {
-        let shape: (usize, usize) = shape.extract().unwrap();
+    pub fn get_row(&self, index: usize) -> PyMatrix {
         PyMatrix {
-            matrix: matrix::Matrix::new(vec![value; shape.0 * shape.1], shape),
+            matrix: self.matrix.get_row(index),
         }
     }
 
-    fn __str__(&self) -> String {
+    pub fn get_column(&self, index: usize) -> PyMatrix {
+        PyMatrix {
+            matrix: self.matrix.get_column(index),
+        }
+    }
+
+    pub fn transpose(&self) -> PyMatrix {
+        PyMatrix {
+            matrix: self.matrix.transpose(),
+        }
+    }
+
+    pub fn add(&self, other: &PyMatrix) -> PyMatrix {
+        PyMatrix {
+            matrix: matrix::Matrix::add(&self.matrix, &other.matrix),
+        }
+    }
+
+    pub fn sub(&self, other: &PyMatrix) -> PyMatrix {
+        PyMatrix {
+            matrix: matrix::Matrix::sub(&self.matrix, &other.matrix),
+        }
+    }
+
+    pub fn mul(&self, other: &PyMatrix) -> PyMatrix {
+        PyMatrix {
+            matrix: matrix::Matrix::mul(&self.matrix, &other.matrix),
+        }
+    }
+
+    pub fn mul_scalar(&self, scalar: f32) -> PyMatrix {
+        PyMatrix {
+            matrix: matrix::Matrix::mul_scalar(&self.matrix, scalar),
+        }
+    }
+
+    pub fn dot(&self, other: &PyMatrix) -> PyMatrix {
+        PyMatrix {
+            matrix: matrix::Matrix::dot(&self.matrix, &other.matrix),
+        }
+    }
+
+    pub fn map(&self, lambda: &Bound<'_, PyFunction>) -> PyMatrix {
+        let f = |x: f32| -> f32 { lambda.call1((x,)).unwrap().extract().unwrap() };
+
+        PyMatrix {
+            matrix: matrix::Matrix::map(&self.matrix, f),
+        }
+    }
+
+    pub fn sum(&self) -> f32 {
+        self.matrix.sum()
+    }
+
+    pub fn __str__(&self) -> String {
         format!("{}", self.matrix)
     }
 }
