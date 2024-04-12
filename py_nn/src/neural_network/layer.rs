ce188dc7d3852917aa7bc0c3c011e2e6ce186753
Fadi Atieh <fadi.atieh98@gmail.com>
2024-04-12 07:07:27 -0500

    Add Layer to bindings.

--- py_nn/src/neural_network/layer.rs
+++ py_nn/src/neural_network/layer.rs
@@ -8,6 +8,38 @@ pub struct Layer {
 
 #[pymethods]
 impl Layer {
+    #[staticmethod]
+    pub fn new_dense(shape: (usize, usize)) -> Self {
+        Layer {
+            layer: Box::new(layer::fully_connected::FullyConnectedLayer::new(
+                shape,
+                &mut rand::thread_rng(),
+            )),
+        }
+    }
+
+    #[staticmethod]
+    pub fn new_activation(activation_function: String) -> Self {
+        match activation_function.as_str() {
+            "relu" => Layer {
+                layer: Box::new(layer::activation::ActivationLayer::new(
+                    layer::activation::ActivationFunction::ReLU,
+                )),
+            },
+            "sigmoid" => Layer {
+                layer: Box::new(layer::activation::ActivationLayer::new(
+                    layer::activation::ActivationFunction::Sigmoid,
+                )),
+            },
+            "tanh" => Layer {
+                layer: Box::new(layer::activation::ActivationLayer::new(
+                    layer::activation::ActivationFunction::Tanh,
+                )),
+            },
+            _ => panic!("Invalid activation function {}.", activation_function),
+        }
+    }
+
     pub fn forward(&mut self, input: &crate::matrix::Matrix) -> crate::matrix::Matrix {
         crate::matrix::Matrix {
             matrix: self.layer.forward(&input.matrix),
