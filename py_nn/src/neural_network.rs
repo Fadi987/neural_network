3dd955495713c714cdceab8d18833ddf06bdb8ca
Fadi Atieh <fadi.atieh98@gmail.com>
2024-04-14 09:31:25 -0500

    Expose fully connected/activation layers.

--- py_nn/src/neural_network.rs
+++ py_nn/src/neural_network.rs
@@ -2,7 +2,7 @@ use crate::matrix;
 use nn_core::neural_network;
 use pyo3::prelude::*;
 
-pub mod layer;
+// pub mod layer;
 
 #[pyclass]
 pub struct NeuralNetwork {
@@ -16,6 +16,33 @@ impl NeuralNetwork {
         NeuralNetwork {
             neural_network: neural_network::NeuralNetwork::new(),
         }
+    }
+
+    pub fn add_fully_connected_layer(&mut self, shape: (usize, usize)) {
+        let fully_connected_layer =
+            neural_network::layer::fully_connected::FullyConnectedLayer::new(
+                shape,
+                &mut rand::thread_rng(),
+            );
+
+        self.neural_network.add_layer(fully_connected_layer);
+    }
+
+    pub fn add_activation_layer(&mut self, activation_function: String) {
+        let activation_layer = match activation_function.as_str() {
+            "relu" => neural_network::layer::activation::ActivationLayer::new(
+                neural_network::layer::activation::ActivationFunction::ReLU,
+            ),
+            "sigmoid" => neural_network::layer::activation::ActivationLayer::new(
+                neural_network::layer::activation::ActivationFunction::Sigmoid,
+            ),
+            "tanh" => neural_network::layer::activation::ActivationLayer::new(
+                neural_network::layer::activation::ActivationFunction::Tanh,
+            ),
+            _ => panic!("Invalid activation function {}.", activation_function),
+        };
+
+        self.neural_network.add_layer(activation_layer);
     }
 
     pub fn forward(&mut self, input: &matrix::Matrix) -> matrix::Matrix {
